5bb6ba49bffe0701920217d3c75b456ea05a93ff
lksarmiento67 <lksarmiento67@gmail.com>
2025-01-22 22:54:28 -0300

    first commit

---
+++ js/google-analytics.js
@@ -0,0 +1,7 @@
+// Google Analytics 4 (GA4) Configuration
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+// Reemplaza 'G-XXXXXXXXXX' con tu ID de medición real cuando lo tengas
+gtag('config', 'G-XXXXXXXXXX');
