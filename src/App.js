e26e5bfba0414a74f118d52058a5dca65a70c0aa
lksarmiento67 <lksarmiento67@gmail.com>
2025-02-17 21:32:41 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import logo from '../public/logos-he-imagenes/logo-sell.png'; // Importa tu logo
 
 function App() {
   return (
     <div className="App">
-      {/* Asegúrate de que no haya referencias al logo de React aquí */}
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={logo} className="App-logo" alt="logo" /> {/* Usa tu logo */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
