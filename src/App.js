25329b02bca3aa6a6bf86571b03a9f926e4c0707
lksarmiento67 <lksarmiento67@gmail.com>
2025-06-29 02:37:42 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/logo-sell.png'; // Asegúrate de que la ruta sea correcta
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> {/* Usa tu logo */}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; 
